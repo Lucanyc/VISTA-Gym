503089b6620e19b591a8a93dbb396903624b2415
Meng@vt <109017164+Lucanyc@users.noreply.github.com>
2026-03-03 21:21:54 -0500

    Update and rename qwen25vl_full_7b.sh to internvl3_8b_dynamic_res_2nd_finetune_full.sh

--- configs/internvl3_8b_dynamic_res_2nd_finetune_full.sh
+++ configs/internvl3_8b_dynamic_res_2nd_finetune_full.sh
@@ -1,28 +1,27 @@
-#!/bin/bash
-
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 set -x
 
-GPUS=8
-BATCH_SIZE=128
-PER_DEVICE_BATCH_SIZE=2
+GPUS=${GPUS:-8}
+BATCH_SIZE=${BATCH_SIZE:-128}
+PER_DEVICE_BATCH_SIZE=${PER_DEVICE_BATCH_SIZE:-4}
 GRADIENT_ACC=$((BATCH_SIZE / PER_DEVICE_BATCH_SIZE / GPUS))
 
-export PYTHONPATH="${PYTHONPATH:-}:$(pwd)"
-export MASTER_PORT=29503
+
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
+export MASTER_PORT=34229
+export TF_CPP_MIN_LOG_LEVEL=3
 export LAUNCHER=pytorch
 
-# NCCL 配置
-export NCCL_NVLS_ENABLE=0
-export NCCL_IB_DISABLE=1
-export NCCL_P2P_DISABLE=0
-export NCCL_DEBUG=WARN
-export CUDA_LAUNCH_BLOCKING=0
-export TF_CPP_MIN_LOG_LEVEL=3
+OUTPUT_DIR='work_dirs/internvl_chat_v3/internvl3_8b_dynamic_res_2nd_finetune_full'
 
-OUTPUT_DIR='/data/sft-model'
-mkdir -p "$OUTPUT_DIR"
+if [ ! -d "$OUTPUT_DIR" ]; then
+  mkdir -p "$OUTPUT_DIR"
+fi
 
+# number of gpus: 8
+# batch size per gpu: 4
+# gradient accumulation steps: 4
+# total batch size: 128
+# epoch: 1
 torchrun \
   --nnodes=1 \
   --node_rank=0 \
@@ -30,14 +29,14 @@ torchrun \
   --nproc_per_node=${GPUS} \
   --master_port=${MASTER_PORT} \
   internvl/train/internvl_chat_finetune.py \
-  --model_name_or_path "/data/InternVL3-8B" \
+  --model_name_or_path "OpenGVLab/InternVL3-8B" \
   --conv_style "internvl2_5" \
   --use_fast_tokenizer False \
   --output_dir ${OUTPUT_DIR} \
-  --meta_path "/data/examples.json" \
-  --overwrite_output_dir True \
+  --meta_path "./shell/data/internvl_1_2_finetune_custom.json" \
+ --overwrite_output_dir True \
   --force_image_size 448 \
-  --max_dynamic_patch 6 \
+  --max_dynamic_patch 12 \
   --down_sample_ratio 0.5 \
   --drop_path_rate 0.0 \
   --freeze_llm False \
@@ -46,18 +45,19 @@ torchrun \
   --vision_select_layer -1 \
   --dataloader_num_workers 4 \
   --bf16 True \
-  --num_train_epochs 3 \
+  --num_train_epochs 1 \
   --per_device_train_batch_size ${PER_DEVICE_BATCH_SIZE} \
   --gradient_accumulation_steps ${GRADIENT_ACC} \
   --evaluation_strategy "no" \
-  --save_strategy "epoch" \
-  --save_total_limit 3 \
+  --save_strategy "steps" \
+  --save_steps 200 \
+  --save_total_limit 1 \
   --learning_rate 2e-5 \
   --weight_decay 0.05 \
   --warmup_ratio 0.03 \
   --lr_scheduler_type "cosine" \
   --logging_steps 1 \
-  --max_seq_length 14384 \
+  --max_seq_length 16384 \
   --do_train True \
   --grad_checkpoint True \
   --group_by_length True \
